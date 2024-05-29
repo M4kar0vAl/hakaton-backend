d4f0678130599b44f7296d25394757add14f668d
Alexey <alex.makarov2290@gmail.com>
2024-05-29 16:52:35 +0300

    removed django comment from settings

--- core/config/settings.py
+++ core/config/settings.py
@@ -95,9 +95,6 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = (
