cb823af9d028ed8d96f4e1bcad517e967cbbff4b
Alexey <alex.makarov2290@gmail.com>
2024-06-05 20:53:48 +0300

    removed comments and prints from middleware

--- core/apps/chat/middleware.py
+++ core/apps/chat/middleware.py
@@ -7,7 +7,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 import jwt
-from channels.auth import AuthMiddlewareStack
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from channels.db import database_sync_to_async
@@ -23,21 +22,16 @@ ALGORITHM = "HS256"
 def get_user(token):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=ALGORITHM)
-        print('payload', payload)
     except:
-        print('no payload')
         return AnonymousUser()
 
     token_exp = datetime.fromtimestamp(payload['exp'])
     if token_exp < datetime.utcnow():
-        print("no date-time")
         return AnonymousUser()
 
     try:
         user = User.objects.get(id=payload['user_id'])
-        print('user', user)
     except User.DoesNotExist:
-        print('no user')
         return AnonymousUser()
 
     return user
@@ -52,17 +46,10 @@ class TokenAuthMiddleware(BaseMiddleware):
             token_key = (dict((x.split('=') for x in scope['query_string'].decode().split("&")))).get('token', None)
         except ValueError:
             token_key = None
-        # try:
-        #     token_key = dict(scope['headers'])[b'sec-websocket-protocol'].decode('utf-8')
-        #     print('d1', token_key)
-        # except ValueError:
-        #     token_key = None
 
         scope['user'] = await get_user(token_key)
-        print('d2', scope['user'])
         return await super().__call__(scope, receive, send)
 
 
 def JwtAuthMiddlewareStack(inner):
     return TokenAuthMiddleware(inner)
-    # return TokenAuthMiddleware(AuthMiddlewareStack(inner))
