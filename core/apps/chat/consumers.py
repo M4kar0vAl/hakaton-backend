f6f8ab4aa02e9a36994adaf7bd0edae28c163ed4
Alexey <alex.makarov2290@gmail.com>
2024-06-05 18:03:10 +0300

    changed: redefine reply method in RoomConsumer

--- core/apps/chat/consumers.py
+++ core/apps/chat/consumers.py
@@ -3,7 +3,6 @@ from djangochannelsrestframework.decorators import action
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 
 from core.apps.brand.models import Brand
-from core.apps.brand.serializers import BrandGetSerializer
 from core.apps.chat.models import Room, Message
 from core.apps.chat.serializers import RoomSerializer
 
@@ -16,6 +15,8 @@ class RoomConsumer(GenericAsyncAPIConsumer):
     async def connect(self):
         self.room_pk = self.scope["url_route"]["kwargs"]["pk"]
         self.room_group_name = f'room_{self.room_pk}'
+        self.brand = self.get_brand()
+        self.room = self.get_room()
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
@@ -24,21 +25,37 @@ class RoomConsumer(GenericAsyncAPIConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
+    # переопределил reply, чтобы он отправлял данные всем в группе
+    # (по умолчанию, вроде, возвращает только тому, кто отправил на сервер)
+    async def reply(
+            self, action: str, data=None, errors=None, status=200, request_id=None
+    ):
+        if errors is None:
+            errors = []
+
+        payload = {
+            "errors": errors,
+            "data": data,
+            "action": action,
+            "response_status": status,
+            "request_id": request_id,
+        }
+
+        await self.channel_layer.group_send(self.room_group_name, payload)
+
     @action()
     async def join_room(self, pk, **kwargs):
-        self.room_subscribe = pk
-        await self.add_brand_to_room(pk)
+        self.add_brand_to_room(pk)
 
     @action()
     async def leave_room(self, pk, **kwargs):
-        await self.remove_brand_from_room(pk)
+        self.remove_brand_from_room(pk)
 
     @action()
     async def create_message(self, message, **kwargs):
-        room = await self.get_room(pk=self.room_subscribe)
         await database_sync_to_async(Message.objects.create)(
-            room=room,
-            user=self.scope["user"],
+            room=self.room,
+            user=self.brand,
             text=message
         )
 
@@ -47,19 +64,15 @@ class RoomConsumer(GenericAsyncAPIConsumer):
         return Room.objects.get(pk=pk)
 
     @database_sync_to_async
-    def current_brands(self, room: Room):
-        return [BrandGetSerializer(brand).data for brand in room.participants.all()]
-
-    @database_sync_to_async
     def remove_brand_from_room(self, room_pk):
-        self.get_brand().rooms.remove(room_pk)
+        self.brand.rooms.remove(room_pk)
 
     @database_sync_to_async
-    def add_brand_to_room(self, pk):
-        brand = self.get_brand()
-        if not brand.rooms.filter(pk=pk).exists():
-            brand.rooms.add(self.get_room(pk=pk))
+    def add_brand_to_room(self, room_pk):
+        brand = self.brand
+        if not brand.rooms.filter(pk=room_pk).exists():
+            brand.rooms.add(room_pk)
 
     @database_sync_to_async
-    def get_brand(self):
+    def get_brand(self) -> Brand:
         return Brand.objects.get(user=self.scope['user'])
