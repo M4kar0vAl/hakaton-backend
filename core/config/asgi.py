748bafa44a8da348f1457d2919e40142a52a77b1
Alexey <alex.makarov2290@gmail.com>
2024-06-05 15:44:41 +0300

    fix: asgi import

--- core/config/asgi.py
+++ core/config/asgi.py
@@ -14,15 +14,16 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from core.apps import chat
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.config.settings')
 
 django_asgi_app = get_asgi_application()
 
+# импорт обязательно после get_asgi_application
+from core.apps.chat import routing
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
     ),
 })
