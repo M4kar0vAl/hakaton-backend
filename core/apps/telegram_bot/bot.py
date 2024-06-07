a98609490a2bff02e6ca1324c3442f6b90c0a097
danii <denisss19971@rambler.ru>
2024-06-07 10:49:36 +0300

    Added some more simple functions for w2w bot and conf

--- core/apps/telegram_bot/bot.py
+++ core/apps/telegram_bot/bot.py
@@ -1,21 +1,27 @@
 import telebot
-
-
+import webbrowser
+from conf import url
 
 bot = telebot.TeleBot('6991307584:AAH3-fJH1FVuAP8xjCyU8iNudzRf-Wz1pew',parse_mode=None)
 
 
-@bot.message_handler(commands= ['start'])
-def command_handler(message: telebot.types.Message):
-    text = f"""Добро пожаловать в бот комопании  W2W. Данный бот служит для оповещения о новых анкетах  коллабораций. Для получения более детальной информации о командах введите /help"""
+@bot.message_handler(commands= ['start','main','hello'])
+def start_handler(message: telebot.types.Message):
+    text = f"""Привет {message.chat.first_name}! Добро пожаловать в бот комопании  W2W. Данный бот служит для оповещения о новых анкетах  коллабораций. Для получения более детальной информации о командах введите команду /help"""
     bot.reply_to(message,text)
 
 @bot.message_handler(commands= ['help'])
-def command_handler(message: telebot.types.Message):
-    text = f"""Возможности"""
+def help_handler(message: telebot.types.Message):
+    text = f"""Возможности:"""
     bot.reply_to(message,text)
 
+@bot.message_handler(commands = ['site','website'])
+def open_site(message:telebot.types.Message):
+    webbrowser.open(f'{url}')
 
-
+@bot.message_handler()
+def hello(message:telebot.types.Message):
+    if message.text.lower() == 'hello' or message.text.lower() == 'привет':
+        bot.reply_to(message, 'Привет!')
 
 bot.polling(none_stop=True)
