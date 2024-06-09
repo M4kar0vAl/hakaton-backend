de09f3a8ef74b75ef6da1d4c3e42182f1abea6ef
posholokia <ilya.posholokk@gmail.com>
2024-06-09 22:52:31 +0600

    added: redis sub

--- core/apps/telegram_bot/bot.py
+++ core/apps/telegram_bot/bot.py
@@ -1,7 +1,27 @@
 import telebot
-from telebot import types
-from conf import TOKEN
+
+from redis import asyncio as aioredis
+
+from conf import (
+    TOKEN,
+    REDIS_USER,
+    REDIS_PASS,
+    REDIS_HOST,
+    REDIS_PORT,
+)
 
 bot = telebot.TeleBot(f'{TOKEN}',parse_mode=None)
 
-bot.polling(none_stop=True)
+
+async def main():
+    def run_telebot():
+        bot.polling(none_stop=True)
+
+    async def sub_redis():
+        redis = await aioredis.Redis.from_url(
+            (f'redis://{REDIS_USER}:'
+             f'{REDIS_PASS}@'
+             f'{REDIS_HOST}:'
+             f'{REDIS_PORT}/3')
+        )
+        pub = redis.pubsub()
--- core/apps/telegram_bot/conf.py
+++ core/apps/telegram_bot/conf.py
@@ -3,3 +3,8 @@ import os
 print(os.environ.get('TOKEN'))
 URL = 'https://...'
 TOKEN = "6991307584:AAH3-fJH1FVuAP8xjCyU8iNudzRf-Wz1pew"
+
+REDIS_USER = os.environ.get('REDIS_USER')
+REDIS_PASS = os.environ.get('REDIS_PASS')
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
