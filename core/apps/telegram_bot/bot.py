d343d6061adb64c4fbe8fbcce69816772f9a8d33
posholokia <ilya.posholokk@gmail.com>
2024-06-09 23:18:49 +0600

    added: redis sub

--- core/apps/telegram_bot/bot.py
+++ core/apps/telegram_bot/bot.py
@@ -1,7 +1,35 @@
 import telebot
-from telebot import types
-import
+import json
+from redis import asyncio as aioredis
+
+from conf import (
+    TOKEN,
+    REDIS_USER,
+    REDIS_PASS,
+    REDIS_HOST,
+    REDIS_PORT,
+)
 
 bot = telebot.TeleBot(f'{TOKEN}',parse_mode=None)
 
-bot.polling(none_stop=True)
+
+async def main():
+    def run_telebot():
+        bot.polling(none_stop=True)
+
+    async def sub_redis():
+        redis = await aioredis.Redis.from_url(
+            (f'redis://{REDIS_USER}:'
+             f'{REDIS_PASS}@'
+             f'{REDIS_HOST}:'
+             f'{REDIS_PORT}/3')
+        )
+        pub = redis.pubsub()
+        await pub.subscribe('organizer')
+        async for msg in pub.listen():
+            data = msg.get('data', None)
+            if type(data) is bytes:  # при первичном подключении в data будет число
+                data_dict = json.loads(data.decode())
+                message = data_dict.get('message')
+                users = data_dict.get('users')
+                await send_notification(users, message)
--- core/apps/telegram_bot/handlers.py
+++ core/apps/telegram_bot/handlers.py
@@ -1,28 +1,42 @@
 import telebot
 from telebot import types
 from bot import bot
-from dotenv import load
+from dotenv import load_dotenv
 
-@bot.message_handler(commands= ['start','main','hello'])
+
+@bot.message_handler(commands=['start', 'main', 'hello'])
 def start_handler(message: telebot.types.Message):
-    text = f"""Привет {message.chat.first_name}! Добро пожаловать в бот комопании  W2W. Данный бот служит для оповещения о новых анкетах  коллабораций. Для получения более детальной информации о командах введите команду /help"""
-    bot.reply_to(message,text)
+    text = (
+        f"Привет {message.chat.first_name}! Добро пожаловать "
+        f"в бот комопании  W2W. Данный бот служит для оповещения "
+        f"о новых анкетах  коллабораций. Для получения более "
+        f"детальной информации о командах введите команду /help"
+    )
+    bot.reply_to(message, text)
 
-@bot.message_handler(commands= ['help'])
+
+@bot.message_handler(commands=['help'])
 def help_handler(message: telebot.types.Message):
     text = f"""Возможности:"""
-    bot.reply_to(message,text)
+    bot.reply_to(message, text)
 
-@bot.message_handler(commands = ['site','website'])
+
+@bot.message_handler(commands=['site', 'website'])
 def open_site(message: telebot.types.Message):
     button = types.InlineKeyboardMarkup()
-    button.add(types.InlineKeyboardButton('Перейти на сайт',url ='https://google.com')) #будет добавлен url нашего приложения
-    bot.reply_to(message,'Перейти нас сайт', reply_markup=button)
+    button.add(
+        types.InlineKeyboardButton(
+            'Перейти на сайт', url='https://google.com'
+        )
+    )  # будет добавлен url нашего приложения
+    bot.reply_to(message, 'Перейти нас сайт', reply_markup=button)
+
 
 @bot.message_handler()
-def hello(message:telebot.types.Message):
+def hello(message: telebot.types.Message):
     if message.text.lower() == 'hello' or message.text.lower() == 'привет':
         bot.reply_to(message, 'Привет!')
+
 
 @bot.message_handler()
 def spam(spam_message, id_list):
