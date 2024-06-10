f70ab3373e7920f22997357700339fa828f6564d
Alexey <alex.makarov2290@gmail.com>
2024-06-10 23:40:06 +0300

    added: last_message field to RoomSerializer

--- core/apps/chat/serializers.py
+++ core/apps/chat/serializers.py
@@ -6,10 +6,14 @@ from core.apps.chat.models import Room, Message
 
 class RoomSerializer(serializers.ModelSerializer):
     participants = BrandGetSerializer(many=True)
+    last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
         exclude = []
+
+    def get_last_message(self, obj):
+        return MessageSerializer(obj.messages.order_by('created_at').last()).data
 
 
 class MessageSerializer(serializers.ModelSerializer):
