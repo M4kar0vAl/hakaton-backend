dffdc906de32a69c4a21afca9c35f838ea6b3b7d
Alexey <alex.makarov2290@gmail.com>
2024-11-15 18:02:28 +0300

    change: updated debug setting

--- core/config/settings.py
+++ core/config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'w2w-backend.onrender.com']
 
