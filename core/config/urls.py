e833963f8ebc0aa1e04a12af1d764b13b55cca01
Alexey <alex.makarov2290@gmail.com>
2024-11-15 19:04:07 +0300

    change: updated urls to serve media files in production

--- core/config/urls.py
+++ core/config/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, re_path
 from django.urls import path
+from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -22,3 +23,10 @@ urlpatterns = [
 # Нужно чтобы изображения загруженные пользователями корректно отображались при DEBUG = True
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # need for media files to be loaded from server in production
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
