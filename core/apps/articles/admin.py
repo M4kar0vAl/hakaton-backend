44dc93092ed3e98c0ad294ca0ffc3e56dab15545
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-16 20:20:24 +0300

    add: TutorialAdmin

--- core/apps/articles/admin.py
+++ core/apps/articles/admin.py
@@ -1,5 +1,42 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
-from core.apps.articles.models import Article
+from core.apps.articles.models import Article, Tutorial
+from core.common.admin import SearchByIdMixin
 
 admin.site.register(Article)
+
+
+@admin.register(Tutorial)
+class TutorialAdmin(SearchByIdMixin, admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = ['id', 'title', 'excerpt', 'is_published']
+    list_display_links = ['title']
+    search_fields = ['title', 'excerpt']
+    search_help_text = 'ID, title or excerpt'
+    list_filter = ['is_published']
+    list_per_page = 100
+    actions = ['publish', 'unpublish']
+
+    fieldsets = [
+        (None, {'fields': ('id', 'is_published'), 'classes': ['wide', ]}),
+        ('Preview', {'fields': ('title', 'excerpt', 'preview_video'), 'classes': ['wide', ]}),
+        ('Content', {'fields': ('body',), 'classes': ['wide', ]}),
+    ]
+
+    @admin.action(description='Publish selected tutorials')
+    def publish(self, request, queryset):
+        count = queryset.update(is_published=True)
+        self.message_user(
+            request,
+            f'Published {count} objects. Now users can see them!',
+            messages.WARNING
+        )
+
+    @admin.action(description='Unpublish selected tutorials')
+    def unpublish(self, request, queryset):
+        count = queryset.update(is_published=False)
+        self.message_user(
+            request,
+            f'Unpublished {count} objects. Now users cannot see them!',
+            messages.WARNING
+        )
