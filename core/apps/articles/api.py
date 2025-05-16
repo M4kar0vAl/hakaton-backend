87711ae85563e19df2645a51e9a42893c017a67e
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-16 18:09:37 +0300

    add: permissions for tinymce file upload API

--- core/apps/articles/api.py
+++ core/apps/articles/api.py
@@ -1,11 +1,13 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 
+from core.apps.articles.permissions import IsStaff
 from core.apps.articles.serializers import ArticleFileCreateSerializer
 
 
 class ArticleFileUploadView(generics.CreateAPIView):
     serializer_class = ArticleFileCreateSerializer
+    permission_classes = [IsStaff]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
