c1d1c7eab900dae83f9b3103a072119a73496100
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-18 19:34:44 +0300

    add: BaseArticleAdmin, CommunityArticleAdmin

--- core/apps/articles/admin.py
+++ core/apps/articles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin, messages
 
-from core.apps.articles.models import Article, Tutorial
+from core.apps.articles.models import Article, Tutorial, CommunityArticle
 from core.common.admin import SearchByIdMixin
 
 admin.site.register(Article)
@@ -40,3 +40,43 @@ class TutorialAdmin(SearchByIdMixin, admin.ModelAdmin):
             f'Unpublished {count} objects. Now users cannot see them!',
             messages.WARNING
         )
+
+
+class BaseArticleAdmin(SearchByIdMixin, admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = ['id', 'title', 'excerpt', 'is_published']
+    list_display_links = ['title']
+    search_fields = ['title', 'excerpt']
+    search_help_text = 'ID, title or excerpt'
+    list_filter = ['is_published']
+    list_per_page = 100
+    actions = ['publish', 'unpublish']
+
+    fieldsets = [
+        (None, {'fields': ('id', 'is_published'), 'classes': ['wide', ]}),
+        ('Preview', {'fields': ('title', 'excerpt', 'preview'), 'classes': ['wide', ]}),
+        ('Content', {'fields': ('body',), 'classes': ['wide', ]}),
+    ]
+
+    @admin.action(description='Publish selected articles')
+    def publish(self, request, queryset):
+        count = queryset.update(is_published=True)
+        self.message_user(
+            request,
+            f'Published {count} objects. Now users can see them!',
+            messages.WARNING
+        )
+
+    @admin.action(description='Unpublish selected articles')
+    def unpublish(self, request, queryset):
+        count = queryset.update(is_published=False)
+        self.message_user(
+            request,
+            f'Unpublished {count} objects. Now users cannot see them!',
+            messages.WARNING
+        )
+
+
+@admin.register(CommunityArticle)
+class CommunityArticleAdmin(BaseArticleAdmin):
+    pass
