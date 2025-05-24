db989ee4d82ab93cbf6fb7f91e1370a966926810
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-24 17:55:12 +0300

    add: PasswordRecoveryToken factories

--- core/apps/accounts/factories.py
+++ core/apps/accounts/factories.py
@@ -1,6 +1,14 @@
+from datetime import timedelta
+
 import factory
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.models import F
+from factory import post_generation
 from factory.django import DjangoModelFactory
+
+from core.apps.accounts.models import PasswordRecoveryToken
+from core.apps.accounts.utils import get_recovery_token_hash, get_recovery_token
 
 User = get_user_model()
 
@@ -27,3 +35,31 @@ class AdminUserFactory(StaffUserFactory):
     email = factory.Sequence(lambda n: f'admin{n}@example.com')
     fullname = 'Админов Админ Админович'
     is_superuser = True
+
+
+class PasswordRecoveryTokenFactory(DjangoModelFactory):
+    class Meta:
+        model = PasswordRecoveryToken
+        django_get_or_create = ('user',)
+
+    user = factory.SubFactory(UserFactory)
+    token = factory.LazyFunction(lambda: get_recovery_token_hash(get_recovery_token()))
+
+
+class PasswordRecoveryTokenExpiredFactory(PasswordRecoveryTokenFactory):
+    @post_generation
+    def created(self, create, extracted, **kwargs):
+        # if obj was not created, does nothing
+        if not create:
+            return
+
+        # if created was passed to the factory as a parameter,
+        # sets object created value to that one
+        if extracted:
+            self.created = extracted
+        # if token is already expired, does nothing
+        elif self.is_expired:
+            return
+        # otherwise sets created such a way that token is expired now
+        else:
+            self.created = F('created') - timedelta(seconds=settings.PASSWORD_RESET_TIMEOUT)
