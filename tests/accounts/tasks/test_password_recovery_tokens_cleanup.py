edca5e4dcdb156737d44c026d974245a88ea482b
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-24 17:57:42 +0300

    change: updated accounts tasks tests using factories

--- tests/accounts/tasks/test_password_recovery_tokens_cleanup.py
+++ tests/accounts/tasks/test_password_recovery_tokens_cleanup.py
@@ -1,10 +1,7 @@
-from datetime import timedelta
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.db.models import F
 from django.test import TestCase, override_settings
 
+from core.apps.accounts.factories import PasswordRecoveryTokenFactory, PasswordRecoveryTokenExpiredFactory
 from core.apps.accounts.models import PasswordRecoveryToken
 from core.apps.accounts.tasks import password_recovery_tokens_cleanup
 
@@ -18,27 +15,8 @@ User = get_user_model()
 class PasswordRecoveryTokensCleanupTaskTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user1, cls.user2 = User.objects.bulk_create([
-            User(
-                email=f'user{i}@example.com',
-                phone='+79993332211',
-                fullname='Юзеров Юзер Юзерович',
-                password='Pass!234',
-                is_active=True
-            )
-            for i in range(2)
-        ])
-
-        cls.recovery_token, cls.expired_recovery_token = PasswordRecoveryToken.objects.bulk_create([
-            PasswordRecoveryToken(
-                user=user,
-                token='token'
-            )
-            for user in [cls.user1, cls.user2]
-        ])
-
-        cls.expired_recovery_token.created = F('created') - timedelta(seconds=settings.PASSWORD_RESET_TIMEOUT)
-        cls.expired_recovery_token.save()
+        cls.recovery_token = PasswordRecoveryTokenFactory()
+        cls.expired_recovery_token = PasswordRecoveryTokenExpiredFactory()
 
         cls.task = password_recovery_tokens_cleanup
 
