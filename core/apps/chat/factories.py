5bd5aa41c7d3c944d29838941ed10891ea8b9044
Alexey Makarov <alex.makarov2290@gmail.com>
2025-06-15 20:28:05 +0300

    change: updated chat factories, added async versions of some factories

--- core/apps/chat/factories.py
+++ core/apps/chat/factories.py
@@ -1,33 +1,13 @@
 from random import randint
 
 import factory
+from django.conf import settings
+from django.db.models import F
 from factory.django import DjangoModelFactory
 
 from core.apps.accounts.factories import UserFactory
 from core.apps.chat.models import Room, Message, MessageAttachment, RoomFavorites
-
-
-class MessageAttachmentFactory(DjangoModelFactory):
-    class Meta:
-        model = MessageAttachment
-
-    message = None
-    file = factory.django.FileField()
-
-
-class MessageFactory(DjangoModelFactory):
-    class Meta:
-        model = Message
-
-    text = factory.Faker('paragraph')
-    user = factory.SubFactory(UserFactory)
-    room = factory.SubFactory('core.apps.chat.factories.RoomFactory')
-
-
-class MessageWithAttachmentsFactory(MessageFactory):
-    attachments = factory.RelatedFactoryList(
-        MessageAttachmentFactory, factory_related_name='message', size=lambda: randint(1, 3)
-    )
+from core.common.factories import factory_sync_to_async
 
 
 class RoomFactory(DjangoModelFactory):
@@ -47,16 +27,70 @@ class RoomFactory(DjangoModelFactory):
         self.participants.add(*extracted)
 
 
+RoomAsyncFactory = factory_sync_to_async(RoomFactory)
+
+
 class RoomMatchFactory(RoomFactory):
     type = Room.MATCH
+
+
+RoomMatchAsyncFactory = factory_sync_to_async(RoomMatchFactory)
 
 
 class RoomInstantFactory(RoomFactory):
     type = Room.INSTANT
 
 
+RoomInstantAsyncFactory = factory_sync_to_async(RoomInstantFactory)
+
+
 class RoomSupportFactory(RoomFactory):
     type = Room.SUPPORT
+
+
+RoomSupportAsyncFactory = factory_sync_to_async(RoomSupportFactory)
+
+
+class MessageAttachmentFactory(DjangoModelFactory):
+    class Meta:
+        model = MessageAttachment
+
+    message = None
+    file = factory.django.FileField()
+
+
+MessageAttachmentAsyncFactory = factory_sync_to_async(MessageAttachmentFactory)
+
+
+class MessageAttachmentExpiredFactory(MessageAttachmentFactory):
+    @factory.post_generation
+    def created_at(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        self.created_at = F('created_at') - settings.MESSAGE_ATTACHMENT_DANGLING_LIFE_TIME
+
+
+class MessageFactory(DjangoModelFactory):
+    class Meta:
+        model = Message
+
+    class Params:
+        has_attachments = False
+
+    text = factory.Faker('paragraph')
+    user = factory.SubFactory(UserFactory)
+    room = factory.SubFactory(RoomFactory)
+    attachments = factory.Maybe(
+        'has_attachments',
+        yes_declaration=factory.RelatedFactoryList(
+            MessageAttachmentFactory, factory_related_name='message', size=lambda: randint(1, 3)
+        ),
+        no_declaration=None
+    )
+
+
+MessageAsyncFactory = factory_sync_to_async(MessageFactory)
 
 
 class RoomFavoritesFactory(DjangoModelFactory):
